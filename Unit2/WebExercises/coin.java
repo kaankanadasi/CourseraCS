75ee5996f1b79dd10658a3274bcc85b6ba742244
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-11-21 13:12:46 +0300

    Create coin.java

---
+++ Unit2/WebExercises/coin.java
@@ -0,0 +1,18 @@
+/**
+ * Alice tosses a fair coin until she sees two consecutive heads. Bob tosses another fair coin until he sees a head 
+ * followed by a tail. Write a program to estimate the probability that Alice will make fewer tosses than Bob? 
+ * Solution: 39/121
+ * @author Kaan Kanadasi
+ * @since 21 nov 2023
+ */
+
+ public class coin {
+    public static void main(String[] args) {
+        int rand = (int)Math.random()*2; // either 0 (heads) or 1 (tails)
+
+        // two consecutive heads
+        // head followed by a tail
+
+
+    }
+ }
