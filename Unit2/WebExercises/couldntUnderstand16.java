24c4fd2986513236049346766bdf17ddf5d17dfa
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-11-21 21:57:45 +0300

    Update couldntUnderstand16.java

--- Unit2/WebExercises/couldntUnderstand16.java
+++ Unit2/WebExercises/couldntUnderstand16.java
@@ -1,3 +1,5 @@
+// this anda
+
 class couuldntUnderstant16 {
 c = 0;
 while (b > 0) {
@@ -6,3 +8,23 @@ while (b > 0) {
     a = a + a;
 }
 }
+
+// this
+/**
+ * Alice tosses a fair coin until she sees two consecutive heads. Bob tosses another fair coin until he sees a head 
+ * followed by a tail. Write a program to estimate the probability that Alice will make fewer tosses than Bob? 
+ * Solution: 39/121
+ * @author Kaan Kanadasi
+ * @since 21 nov 2023
+ */
+
+ public class coin {
+    public static void main(String[] args) {
+        int rand = (int)Math.random()*2; // either 0 (heads) or 1 (tails)
+
+        // two consecutive heads
+        // head followed by a tail
+
+
+    }
+ }
