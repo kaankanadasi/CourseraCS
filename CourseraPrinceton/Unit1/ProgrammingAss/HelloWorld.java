27fabf1301c8df682edb39189e0c546062394e93
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-11-26 12:13:17 +0300

    Create HelloWorld.java

---
+++ CourseraPrinceton/Unit1/ProgrammingAss/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Kaan Kanadasi
+ * @since 22 nov 2023
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
